b8e1a524cd8253d2711fe5d8d47cab294353a461
Ved Prakash <vedprakashsigh@gmail.com>
2024-07-07 18:48:56 +0530

    changed title and configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import { ProjectProvider } from "./context/ProjectContext.tsx";
+import App from "./App.tsx";
 
+import theme from "./theme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <AuthProvider>
+      <ProjectProvider>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      </ProjectProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
