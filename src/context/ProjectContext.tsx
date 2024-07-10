6757b68aeea8530b90a8a54450c69aeb42a4a982
Ved Prakash <vedprakashsigh@gmail.com>
2024-07-10 12:25:39 +0530

    made modifications to projectContext

--- src/context/ProjectContext.tsx
+++ src/context/ProjectContext.tsx
@@ -1,8 +1,35 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useAuth } from "./AuthContext";
+import config from "../config";
+import { useModal } from "./ModalContext";
+
+export interface Issue {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  project_id: number;
+}
+
+export interface Project {
+  id: number;
+  name: string;
+  description: string;
+  user_id: number;
+  issues: Issue[] | null;
+}
 
 interface ProjectContextProps {
   selectedProjectId: number | null;
   setSelectedProjectId: (id: number | null) => void;
+  projects: Project[] | null;
+  setProjects: (projects: Project[] | null) => void;
 }
 
 const ProjectContext = createContext<ProjectContextProps | undefined>(
@@ -23,10 +50,42 @@ export const ProjectProvider: React.FC<{ children: ReactNode }> = ({
   const [selectedProjectId, setSelectedProjectId] = useState<number | null>(
     null
   );
+  const [projects, setProjects] = useState<Project[] | null>(null);
+  const { state } = useAuth();
+  const { modals } = useModal();
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        if (state?.user?.user_id && state?.token) {
+          const apiUrl = `${config.apiUrl}/api/projects`;
+          const response = await fetch(
+            `${apiUrl}?user_id=${state.user.user_id}`,
+            {
+              headers: {
+                Authorization: `Bearer ${state.token}`,
+              },
+            }
+          );
+
+          if (!response.ok) {
+            throw new Error("Failed to fetch projects");
+          }
+
+          const fetchedProjects = await response.json();
+          setProjects(fetchedProjects);
+        }
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchProjects();
+  }, [state, modals]);
 
   return (
     <ProjectContext.Provider
-      value={{ selectedProjectId, setSelectedProjectId }}
+      value={{ selectedProjectId, setSelectedProjectId, projects, setProjects }}
     >
       {children}
     </ProjectContext.Provider>
