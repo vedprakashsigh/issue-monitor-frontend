a67bae721dbddf9fd71a25396fee72fd6efd19fc
Ved Prakash <vedprakashsigh@gmail.com>
2024-07-20 14:46:55 +0530

    .

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   apiUrl:
     import.meta.env.REACT_APP_API_URL ||
-    // "https://issue-monitor-backend.onrender.com" ||
+    "https://issue-monitor-backend.onrender.com" ||
     "http://127.0.0.1:5000",
   environment: import.meta.env.REACT_APP_ENVIRONMENT,
 };
